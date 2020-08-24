d1c796668a44d1a9446c171066a7f6e2727fac66
Christian Lüdeking <chri30ek@elev.tec.dk>
2020-08-24 12:34:59 +0200

    Added person2 and changed things in "Program"

---
+++ Mandag/Mandag/Person2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOrienteretProg
+{
+    class Person2
+    {
+        // Fields
+        private int age { get; set; }
+        private string name { get; set; }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
--- Mandag/Mandag/Program.cs
+++ Mandag/Mandag/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectOrienteretProg
 {
@@ -7,9 +8,20 @@ namespace ObjectOrienteretProg
         static void Main(string[] args)
         {
             Person p1 = new Person();
+            Person2 p2 = new Person2();
 
             p1.setName("John Johnson");
             p1.setAge(55);
+
+            Person2 p3 = new Person2() { Age = 42, Name="James" };
+
+            List<int> listInt = new List<int>();
+            listInt.Add(4);
+
+            List<Person2> listPerson2 = new
+                List<Person2>();
+            listPerson2.Add(p2);
+            listPerson2.Add(p3);
 
             Console.WriteLine($"p1 objekt navn: {p1.getName()}");
             Console.ReadKey();
