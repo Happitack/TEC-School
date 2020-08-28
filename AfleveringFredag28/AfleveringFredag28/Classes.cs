b46cd170ca8be4787400bdf747ba1d13e8d7b477
Christian Lüdeking <chri30ek@elev.tec.dk>
2020-08-28 09:21:27 +0200

    Initial push

---
+++ AfleveringFredag28/AfleveringFredag28/Classes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AfleveringFredag28
+{
+    class Person
+    {
+        string Fname{ get; set; }
+        string Lname { get; set; }
+        int Age { get; set; }
+        string Email { get; set; }
+        string Tele { get; set; }
+
+        public Person(string fname, string lname, int age, string email, string tele)
+        {
+            Fname = fname; Lname = lname; Age = age; Email = email; Tele = tele;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+    class Employee : Person
+    {
+
+    }
+
+    class Customer : Person
+    {
+
+    }
+}
